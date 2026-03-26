5499f3dd76fdd8569be8e859eb37b2e4a6b39e51
Timbius <timofejkozel533@gmail.com>
2026-03-26 20:44:26 +0300

    db: add users table

---
+++ db_schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS barber_shop_db;
+USE barber_shop_db;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  passwordHash VARCHAR(255) NOT NULL,
+  role ENUM('user', 'admin') DEFAULT 'user',
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
